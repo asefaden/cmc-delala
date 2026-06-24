c70bbf56151cfed5c1543e13d018619725645444
Assefa Demeke <assede1122@gmail.com>
2026-06-24 17:16:50 +0300

    update

--- backend/database.js
+++ backend/database.js
@@ -80,7 +80,7 @@ async function initDb() {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: parseInt(process.env.DB_POR, 10) || 3306,
+    port: parseInt(process.env.DB_PORT, 10) || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
